1e8eb072a5a44c6c99a5c4ae27a292e3c049da7c
Andy Farrell <andrew.farrell@experian.com>
2018-11-15 14:50:55 +0000

    Use build files in HTML

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 HASH=$(date +%Y%m%d%H%M%S)
-BASE_FILENAME=build/fovcalc.$HASH
+BASE_FILENAME=fovcalc.$HASH
 JS_FILENAME=$BASE_FILENAME.min.js
 CSS_FILENAME=$BASE_FILENAME.css
 INPUT_JS=(
   fovcalc.js
 )
 echo Minifying these files: "${INPUT_JS[@]}"
-rm $BASE_FILENAME.*
-uglifyjs -m --warn "${INPUT_JS[@]}" > $JS_FILENAME && echo $JS_FILENAME generated
-cp fovcalc.css $CSS_FILENAME
+
+rm build/*
+
+uglifyjs -m --warn "${INPUT_JS[@]}" > build/$JS_FILENAME &&
+  echo $JS_FILENAME generated
+
+cp fovcalc.css build/$CSS_FILENAME &&
+  echo $CSS_FILENAME created
+
+sed -e "s/CSS_FILENAME_PLACEHOLDER/$CSS_FILENAME/;s/JS_FILENAME_PLACEHOLDER/$JS_FILENAME/" fovcalc.html > build/fovcalc.html &&
+  echo Filenames replaced in build/fovcalc.html
